0350143949fe45d7f163d5495f088fb7e0fc00a4
Batman99SD <azozkrg1@gmail.com>
2024-11-28 20:33:56 +0000

    changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,51 +1,25 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ Making changes """
 
 
 def makeChange(coins, total):
-    """ Generate changes needed
+    """ Generate changes needed to meet a given amount total
 
     Args:
-        coins (List): List of the values of the coins.
-        total (int): total amount
+        coins ([List]): [List of the values of the coins in your possession.]
+        total ([int]): [total amount to be met]
     """
     if total <= 0:
         return 0
-    amount = 0
-    tmp = 0
+    check = 0
+    temp = 0
     coins.sort(reverse=True)
     for coin in coins:
-        while amount < total:
-            amount += coin
-            tmp += 1
-        if amount == total:
-            return tmp
-        amount -= coin
-        tmp -= 1
+        while check < total:
+            check += coin
+            temp += 1
+        if check == total:
+            return temp
+        check -= coin
+        temp -= 1
     return -1
-=======
-"""Solution to make changes problem"""
-
-
-def makeChange(coins, total):
-    """
-    Function to make change for a given total
-    """
-    if total <= 0:
-        return 0
-
-    dp = [total + 1] * (total + 1)
-
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-
-        for j in coins:
-
-            if j <= i:
-
-                dp[i] = min(dp[i], dp[i - j] + 1)
-
-    return -1 if dp[total] > total else dp[total]
->>>>>>> fa99d61dc2d5550689adfa9edf0f383acd0d7277
